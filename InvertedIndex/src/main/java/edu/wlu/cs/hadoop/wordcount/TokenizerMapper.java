7d9ab4b9913513b22d28f7ee914ac99976035846
cooperbaird <cooperbaird@coopers-mbp.ad.wlu.edu>
2017-11-06 15:28:15 -0500

    Remove punct & lowercases. Stopwords in progress.

--- InvertedIndex/src/main/java/edu/wlu/cs/hadoop/wordcount/TokenizerMapper.java
+++ InvertedIndex/src/main/java/edu/wlu/cs/hadoop/wordcount/TokenizerMapper.java
@@ -23,11 +23,13 @@ public class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
 
 	private final static IntWritable one = new IntWritable(1);
 	private Text word = new Text();
+	private Normalizer norm = new Normalizer();
 
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		StringTokenizer itr = new StringTokenizer(value.toString());
 		while (itr.hasMoreTokens()) {
 			word.set(itr.nextToken());
+			word = norm.normalize(word);
 			context.write(word, one);
 		}
 	}
